f9b6041f9378dce707007e95d344670108297b86
Kartik Aggarwal <kartik@Kartiks-MacBook-Pro.local>
2023-11-01 17:54:53 +0530

    apply now cta and footer font fix

--- src/pages/careers/index.js
+++ src/pages/careers/index.js
@@ -4,6 +4,10 @@ import { images }from "../../components/images";
 
 const Careers = ({refs:ref}) => {
 
+    const handleApplyNowClick = () => {
+        window.location.href = 'mailto:info@aistra.com';
+    };
+
   return (
     <div ref={ref} className="careers-container">
         <div className="careers-content">
@@ -14,7 +18,7 @@ const Careers = ({refs:ref}) => {
             <div className="careers-desc">
                 Aistra is led by a team of BPO stalwarts and SaaS veterans from across the globe and we’re on a mission to help businesses prepare for, adapt to and adopt AI. We run an open and flat organization. We’re remote first but meet fairly often to make sure that we get to know each other better and also to celebrate our successes. We operate in a constantly evolving space which brings both challenges and rewards. We’re on a constant lookout for talented individuals to join our growing team.
             </div>
-            <div className="careers-btn-container">
+            <div onClick={handleApplyNowClick} className="careers-btn-container">
                 <img className="btn-img" src={images['apply-now-btn.svg']} alt="Apply now" />
             </div>
         </div>
